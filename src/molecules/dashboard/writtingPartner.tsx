537d2e4880d73212ac7e91e571965d36b57dcc31
Durga Parshad <durga.parshad@itechnolabs.tech>
2022-05-09 16:38:35 +0530

    Initial commit

---
+++ src/molecules/dashboard/writtingPartner.tsx
@@ -0,0 +1,23 @@
+export function WrittingPartner() {
+  return (
+    <div className=" mt-20">
+      <div className="flex flex-col items-center gap-3 text-center text-white">
+        <div className=" text-3xl font-normal">Let’s introduce with</div>
+        <div className=" text-gradient pb-2 text-6xl font-bold">
+          Your Writing Partner!
+        </div>
+        <div className=" max-w-xl text-xl font-normal opacity-70">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          purus odio tempor rutrum...
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <img
+          className=" mt-10 w-full max-w-7xl"
+          src="/assets/images/video-thumbnail.png"
+          alt="thumb-nail"
+        />
+      </div>
+    </div>
+  );
+}
